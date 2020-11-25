11c43c20e36ceab1cc2a8b201f8dddf7f6b76746
kisti <kistiyanto354@gmail.com>
2020-11-25 14:33:45 +0700

    heading list item products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from "react"
+import { Container } from "react-bootstrap"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1 className='header'>HEllo World</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomePage />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
