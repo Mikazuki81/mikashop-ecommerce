0e945e525e218ebe2a273245689429a489ad1133
kisti <kistiyanto354@gmail.com>
2020-11-26 13:51:42 +0700

    complete home screen and product

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Col, Row } from "react-bootstrap"
 
 import products from "../products"
+import Product from "../components/Product"
 
 const HomePage = () => {
   return (
@@ -9,8 +10,8 @@ const HomePage = () => {
       <h1>Latest Product</h1>
       <Row>
         {products.map(product => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <h3>{product.name}</h3>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
