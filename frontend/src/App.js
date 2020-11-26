b6356a6b0b454e7827c50aacf7cd85435be654c4
kisti <kistiyanto354@gmail.com>
2020-11-26 14:44:18 +0700

    Implementing react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import HomePage from "./pages/HomePage"
+import ProductPage from "./pages/ProductPage"
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomePage />
+          <Route component={HomePage} path='/' exact />
+          <Route component={ProductPage} path='/product/:id' />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
---
+++ frontend/src/pages/ProductPage.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import products from "../products"
+
+function ProductPage({ match }) {
+  return (
+    <div>
+      <h3>Product Page</h3>
+    </div>
+  )
+}
+
+export default ProductPage
