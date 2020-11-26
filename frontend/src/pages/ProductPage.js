fe2fb60ae9f67c012b5720c6f5153707e87b111e
kisti <kistiyanto354@gmail.com>
2020-11-26 14:51:36 +0700

    Implementing react router

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,12 +1,35 @@
 import React from "react"
-
+import { Link } from "react-router-dom"
+import {
+  Container,
+  Col,
+  Row,
+  ListGroup,
+  ListGroupItem,
+  Card,
+  Button,
+  Image,
+} from "react-bootstrap"
 import products from "../products"
+import Rating from "../components/Rating"
 
 function ProductPage({ match }) {
+  const product = products.find(p => p._id === match.params.id)
+  console.log(product)
   return (
-    <div>
-      <h3>Product Page</h3>
-    </div>
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Back
+      </Link>
+
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}></Col>
+        <Col></Col>
+      </Row>
+    </>
   )
 }
 
