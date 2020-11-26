883ade2839294f1b78f74fb3f9763b4fb1767535
kisti <kistiyanto354@gmail.com>
2020-11-26 14:24:15 +0700

    complete rating component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,10 +11,10 @@ const Header = () => {
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
               <Nav.Link href='/cart'>
-                <i class='fas fa-shopping-cart'></i>cart
+                <i className='fas fa-shopping-cart'></i>cart
               </Nav.Link>
               <Nav.Link href='/login'>
-                <i class='fas fa-user'></i>Sign In
+                <i className='fas fa-user'></i>Sign In
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
