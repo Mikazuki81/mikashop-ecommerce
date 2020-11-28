4d7440b67ce739e32006fde140fbd2527aa5d70f
kisti <kistiyanto354@gmail.com>
2020-11-28 14:33:00 +0700

    Adding Product detail Screen

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -15,7 +15,17 @@ import Rating from "../components/Rating"
 
 function ProductPage({ match }) {
   const product = products.find(p => p._id === match.params.id)
-  console.log(product)
+
+  const {
+    price,
+    description,
+    numReviews,
+    image,
+    rating,
+    name,
+    countInStock,
+  } = product
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
@@ -24,10 +34,49 @@ function ProductPage({ match }) {
 
       <Row>
         <Col md={6}>
-          <Image src={product.image} alt={product.name} />
+          <Image src={image} alt={name} rounded fluid />
         </Col>
-        <Col md={3}></Col>
-        <Col></Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={rating}
+                text={`${numReviews} review${numReviews > 1 ? "s" : " "}`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price : ${price}</ListGroup.Item>
+            <ListGroup.Item>Description : ${description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price</Col>
+                  <Col>
+                    <strong>${price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Stock:</Col>
+                  <Col>{countInStock > 0 ? "In Stock" : "Out Of Stock:("}</Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button disabled={countInStock === 0} className='btn btn-block'>
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </>
   )
