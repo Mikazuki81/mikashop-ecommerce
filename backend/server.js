4263619786cb0e39c993b39aaf7ddde5bb3f2147
kisti <kistiyanto354@gmail.com>
2020-12-04 21:45:24 +0700

    add error hadler and middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import colors from "colors"
 
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+
 import productRoutes from "./routes/productRoutes.js"
 
 dotenv.config()
@@ -17,6 +19,10 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/products", productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
