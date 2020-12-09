c80b2f15da581fca15c55f356a39870100d27d0c
kisti <kistiyanto354@gmail.com>
2020-12-09 19:53:53 +0700

    add store for redux

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,32 +1,36 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import { Col, Row } from "react-bootstrap"
-
+import { useDispatch, useSelector } from "react-redux"
 import Product from "../components/Product"
-import axios from "axios"
+import { listProducts } from "../action/productsAction"
 
 const HomePage = () => {
-  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+  const productList = useSelector(state => state.productList)
+
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get("/api/products")
-
-      setProducts(data)
-    }
-
-    fetchProduct()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h1>Latest Product</h1>
-      <Row>
-        {products.map(product => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading ... </h2>
+      ) : error ? (
+        error.message
+      ) : (
+        <Row>
+          {products.map(product => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
